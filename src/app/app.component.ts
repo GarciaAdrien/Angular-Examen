f60f16a4ad9fca6b3d335c3f4e765ff74a74f1d6
adrien <adriengarciaperso@gmail.com>
2022-05-25 00:55:26 +0200

    Exercice 8

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,9 @@ import {Statistiques } from './models/Statistiques'
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-
+  Supprimer(){
+    this.statistique1.splice(1);
+  };
   ngOnInit(): void {
   }
 /*
@@ -17,14 +19,16 @@ statistique1: Statistiques[] = [
 new Statistiques("fa1f5f40-be3b-11eb-91ec-7f5875ecfb46", "Démographie en Italie", "60M"),
 new Statistiques("fa1f5f40-be3b-11eb-91ec-7f5878982089", "Démographie en France", "90M")];
 constructor() {
-{
-
   setTimeout(() => {
     this.statistique1[0].identifiant = 'test'
     this.statistique1[0].valeur = 'test'
     this.statistique1[0].titre = 'test'
 
   }, 2000);
+
+{
+
+
 
     }
 
@@ -33,4 +37,8 @@ constructor() {
   }
 }
 
+
+function Supprimer() {
+  throw new Error('Function not implemented.');
+}
 
