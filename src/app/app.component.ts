f99cc139a292617e10d33d847b30fb1ff9746c51
adrien <adriengarciaperso@gmail.com>
2022-05-25 00:00:45 +0200

    Exercice 6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,13 +9,22 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
+/*
   @Input() statistique1: Statistiques = new Statistiques("fa1f5f40-be3b-11eb-91ec-7f5875ecfb46", "Démographie en Italie", "60M”");
   @Input() statistique2: Statistiques = new Statistiques("fa1f5f40-be3b-11eb-91ec-7f5878982089", "Démographie en France", "90M”");
-
-  constructor() {
+*/
+statistique1: Statistiques[] = [
+new Statistiques("fa1f5f40-be3b-11eb-91ec-7f5875ecfb46", "Démographie en Italie", "60M"),
+new Statistiques("fa1f5f40-be3b-11eb-91ec-7f5878982089", "Démographie en France", "90M")];
+constructor() {
 {
 
+  setTimeout(() => {
+    this.statistique1[0].identifiant = 'test'
+    this.statistique1[0].valeur = 'test'
+    this.statistique1[0].titre = 'test'
+
+  }, 2000);
 
     }
 
